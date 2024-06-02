bcb25934c52a06ceb92ebf9f9305beae9eec48ce
Péter János Császár <csaszar.peter@outlook.hu>
2024-06-03 01:53:36 +0200

    remove unnecessary files

--- tests/src/test/java/TestQueryObject.java
+++
@@ -1,9 +0,0 @@
-public class TestQueryObject {
-    public String query;
-    public String elem;
-
-    TestQueryObject(String query, String elem) {
-        this.query = query;
-        this.elem = elem;
-    }
-}
